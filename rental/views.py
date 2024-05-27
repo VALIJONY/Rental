c11f9ad6fd57e93a8f5bc8cd443bb98cf5bbbe4e
VALIJONY <valijondosov@gmail.com>
2024-05-27 15:52:15 +0500

    commit

--- rental/views.py
+++ rental/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login,logout
 from django.urls import reverse_lazy
